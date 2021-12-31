d6f2cba42efce6ba13b6bf0aad89c3bbf5be67d9
Nanakra <Nanakra@gmail.com>
2021-12-31 18:40:30 -0500

    Initial

---
+++ 1.3/Source/HediffGiver_AlramX.cs
@@ -0,0 +1,16 @@
+﻿using Verse;
+
+namespace Rakkle
+{
+    public class HediffGiver_AlramX : HediffGiver
+    {
+        public override void OnIntervalPassed(Pawn pawn, Hediff cause)
+        {
+            if (Rand.MTBEventOccurs(this.mtbDays, 60000f, 60f) && base.TryApply(pawn, null))
+            {
+
+            }
+        }
+        public float mtbDays;
+    }
+}
